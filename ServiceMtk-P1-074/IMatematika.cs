bc18a491007946600d18354e9bfbc4b85260ac85
eeRha <arrasyidtz3@gmail.com>
2020-11-07 14:23:19 +0700

    add service

--- ServiceMtk-P1-074/IMatematika.cs
+++ ServiceMtk-P1-074/IMatematika.cs
@@ -19,14 +19,19 @@ namespace ServiceMtk_P1_074
 
         [OperationContract]
         /* output->*/
+        [FaultContract(typeof(MathFault))]
         int Tambah(int a, int b); //method
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kurang(int a, int b); //input
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Kali(int a, int b);
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         int Bagi(int a, int b);
         [OperationContract]
+        [FaultContract(typeof(MathFault))]
         Koordinat TKoordinat(Koordinat a, Koordinat b); //object dari class
 
         // TODO: Add your service operations here
@@ -71,5 +76,14 @@ namespace ServiceMtk_P1_074
             get { return _y; }
             set { _y = value; }
         }
+    }
+
+    [DataContract]
+    class MathFault
+    {
+        [DataMember]
+        public string Kode { get; set; }
+        [DataMember]
+        public string Pesan { get; set; }
     }
 }
